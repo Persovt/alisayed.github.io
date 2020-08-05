a735846700c2a7719d3b64a683a61f166325131f
Persovt <46276626+Persovt@users.noreply.github.com>
2020-08-05 14:15:26 +0300

    second commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,11 +3,6 @@ const burger = document.querySelector(".burger");
 
 const menuOpen = () => {
     navMenu.classList.toggle("active"); 
+    burger.classList.toggle("active"); 
 }
 burger.addEventListener('click', menuOpen);
-    if(navMenu.classList.contains('active') == true)
-        {
-        document.addEventListener('click', () => {
-        if (navMenu.contains(event.target))  navMenu.classList.remove("active"); 
-        })
-}
