5daaf14c59d28b741d01b9036bf7ac4f6d8ec491
Persovt <46276626+Persovt@users.noreply.github.com>
2020-08-05 16:32:19 +0300

    third commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,13 @@
 const navMenu = document.querySelector(".nav-menu");
 const burger = document.querySelector(".burger");
-
+const body     = document.querySelector("body")
 const menuOpen = () => {
+    
+    if(navMenu.classList.contains('active') && !burger.classList.contains('active-burger'))
+        burger.classList.toggle("active-burger"); 
+
     navMenu.classList.toggle("active"); 
-    burger.classList.toggle("active"); 
+    burger.classList.toggle("active-burger"); 
+    body.classList.toggle('is-scroll');
 }
 burger.addEventListener('click', menuOpen);
